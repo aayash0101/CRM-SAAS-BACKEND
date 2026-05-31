71e4edcee49a50150c25222a999bf584ca5dd9c9
Aayash <dangaayash@gmail.com>
2026-05-31 16:52:34 +0545

    express

---
+++ src/common/types/express.d.ts
@@ -0,0 +1,18 @@
+import { Role } from '@prisma/client';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        role: Role;
+        organizationId: string;
+        firstName: string;
+        lastName: string;
+      };
+    }
+  }
+}
+
+export {};
